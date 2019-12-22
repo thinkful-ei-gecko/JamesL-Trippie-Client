4e6d5c98c796e90d802e1eb4ea9d68da8aee36d2
James Lee <jameswlee109@gmail.com>
2019-12-22 08:54:57 -0600

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   // API_ENDPOINT: `http://localhost:8000/api`,
-  API_ENDPOINT: `https://fast-scrubland-10060.herokuapp.com`,
+  API_ENDPOINT: `https://fast-scrubland-10060.herokuapp.com/api`,
   API_KEY: process.env.REACT_APP_API_KEY
 };
